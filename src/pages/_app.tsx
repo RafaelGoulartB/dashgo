e991cb846454947ea5664743b1bd60c6f3a76434
RafaelGoulartB <rafagoulartb@gmail.com>
2021-11-28 18:20:03 -0300

    🎨 styles: adding theme configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import React from 'react'
+import { theme } from '../styles/theme'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
