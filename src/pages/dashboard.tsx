cb9d6d47001dde903ca57d2bcfc6905517836bf4
RafaelGoulartB <rafagoulartb@gmail.com>
2021-12-05 17:03:29 -0300

    📦 feat: creating header with search box

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Headeer/Header';
+
+
+const Dashboard: React.FC = () => {
+  return (
+    <Header />
+  );
+}
+
+export default Dashboard;
