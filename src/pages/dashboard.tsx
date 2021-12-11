e65d0a61433cb849f3120a189d063844fe3d25b8
RafaelGoulartB <rafagoulartb@gmail.com>
2021-12-10 21:19:45 -0300

    📦 feat: creating sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,18 @@
+import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import Header from '../components/Headeer/Header';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 
 const Dashboard: React.FC = () => {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
 
